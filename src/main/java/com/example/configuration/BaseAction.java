fba7f54c7a2241e7f0b9cbaf8a6f5fef997b360b
arnontp <masternongkhonthai@gmail.com>
2017-09-26 22:29:41 +0700

    delete all

--- src/main/java/com/example/configuration/BaseAction.java
+++
@@ -1,7 +0,0 @@
-package com.example.configuration;
-
-public class BaseAction {
-	 public static final int BUTTONS_TO_SHOW = 5;
-	 public static final int[] PAGE_SIZES = { 5, 10, 20 };
-	 public static final int INITIAL_PAGE_SIZE = 5;
-}
